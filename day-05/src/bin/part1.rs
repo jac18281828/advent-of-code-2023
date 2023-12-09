acb8fae5057a3e2d8e16e393448d23b7b66158cf
John Cairns <john@2ad.com>
2023-12-09 15:53:43 -0600

    save place

--- day-05/src/bin/part1.rs
+++ day-05/src/bin/part1.rs
@@ -1,3 +1,42 @@
+use tracing::Level;
+use lyn::Scanner;
+
+struct AlminacParser {
+    scanner: Scanner,
+}
+
+impl AlminacParser {
+    fn new(input: &str) -> Self {
+        Self {
+            scanner: Scanner::new(input),
+        }
+    }
+
+    fn parse(&mut self) -> String {
+        let mut result = String::new();
+        while let Some(token) = self.scanner.next() {
+            result.push_str(&token);
+        }
+        result
+    }
+
+    fn parse_keyword(&mut self) -> String {
+        let mut result = String::new();
+        while let Some(token) = self.scanner.next() {
+            if token == " " {
+                break;
+            }
+            result.push_str(&token);
+        }
+        result
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+    .with_max_level(Level::INFO)
+    .init();
+    let alminac: String = std::io::stdin().lines().map(|l| l.unwrap()).collect();
+    println!("{}", alminac);
 }
